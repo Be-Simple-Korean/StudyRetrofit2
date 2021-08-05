aac228ed08c35ce1d9ca12f59f2d6971a32f7fef
이정현 <cw03125@openobject.net>
2021-08-05 10:44:20 +0900

    commit

--- app/src/main/java/com/example/studyretrofit2/MainActivity.java
+++ app/src/main/java/com/example/studyretrofit2/MainActivity.java
@@ -1,20 +1,23 @@
 package com.example.studyretrofit2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.studyretrofit2.network.NaverAPI;
 import com.example.studyretrofit2.network.vo.MovieDataVO;
 import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import retrofit2.Call;
 import retrofit2.Callback;
-import retrofit2.Converter;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -30,14 +33,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init() {
+        OkHttpClient.Builder header = new OkHttpClient.Builder().addInterceptor(new Interceptor() {
+            @Override
+            public okhttp3.Response intercept(Chain chain) throws IOException {
+                Request request=chain.request().newBuilder()
+                        .addHeader("X-Naver-Client-Id","hhWYTgIoUyV9WAJnjMPm")
+                        .addHeader("X-Naver-Client-Secret","jIa69C_Gn7")
+                        .build();
+                return chain.proceed(request);
+            }
+        });
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://openapi.naver.com")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(header.build())
                 .build();
         NaverAPI naverAPI = retrofit.create(NaverAPI.class);
         Call<MovieDataVO> callMovieData = naverAPI.getMovieTitle(
-                "hhWYTgIoUyV9WAJnjMPm",
-                "jIa69C_Gn7",
+
                 "귀멸",
                 1, 1);
         ImageView iv =findViewById(R.id.iv);
--- app/src/main/java/com/example/studyretrofit2/network/NaverAPI.java
+++ app/src/main/java/com/example/studyretrofit2/network/NaverAPI.java
@@ -4,15 +4,12 @@ import com.example.studyretrofit2.network.vo.MovieDataVO;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Header;
 import retrofit2.http.Query;
 
 public interface NaverAPI {
 
     @GET("/v1/search/movie.json")
     Call<MovieDataVO> getMovieTitle(
-            @Header("X-Naver-Client-Id") String id,
-            @Header("X-Naver-Client-Secret") String secret,
             @Query("query") String query, //검색어
             @Query("display") int display, //결과 출력 개수
             @Query("start") int start
